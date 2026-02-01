9cfb34bb50b1b9d6ac39428aa365fd2883f1850c
Nevin <nevin.castillo@utp.ac.pa>
2026-02-01 16:18:09 -0500

    Agregar archivos de proyecto.

---
+++ SantaSecilia/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SantaSecilia
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
