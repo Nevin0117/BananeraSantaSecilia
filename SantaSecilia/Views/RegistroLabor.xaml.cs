b472f066e4b650bc642895d6e3113db2dca04a1e
Leivadev <luis.leivag23@gmail.com>
2026-02-05 09:38:46 -0500

    Agregar archivos de VIewModel y View para registro laboral

---
+++ SantaSecilia/Views/RegistroLabor.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace SantaSecilia.Views
+{
+    public partial class RegistroLabor : ContentPage
+    {
+        public RegistroLabor()
+        {
+            InitializeComponent();
+        }
+    }
+}
