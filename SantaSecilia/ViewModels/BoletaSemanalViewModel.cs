c1d064a7ef144febff1661c4b1fd4e0e3f2f3b91
DeisyLMQ <starmoreno02@gmail.com>
2026-02-08 17:55:37 -0500

    Cambios en el archivo Boleta ViewModal!!

--- SantaSecilia/ViewModels/BoletaSemanalViewModel.cs
+++ SantaSecilia/ViewModels/BoletaSemanalViewModel.cs
@@ -1,15 +1,75 @@
-namespace SantaSecilia.ViewModels;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
-public class BoletaSemanalViewModel : ContentPage
+namespace SantaSecilia.ViewModels
 {
-	public BoletaSemanalViewModel()
-	{
-		Content = new VerticalStackLayout
-		{
-			Children = {
-				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
-				}
-			}
-		};
-	}
+    public class BoletaFila
+    {
+        public DateTime Fecha { get; set; }
+        public string Actividad { get; set; } = "";
+        public int Horas { get; set; }
+        public decimal Tarifa { get; set; }
+        public decimal Monto => Horas * Tarifa;
+    }
+
+
+    public class BoletaSemanalViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = null!)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public ObservableCollection<string> Trabajadores { get; set; } = new();
+        public ObservableCollection<string> Semanas { get; set; } = new();
+
+        string trabajadorSeleccionado;
+        public string TrabajadorSeleccionado
+        {
+            get => trabajadorSeleccionado;
+            set
+            {
+                trabajadorSeleccionado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        string semanaSeleccionada;
+        public string SemanaSeleccionada
+        {
+            get => semanaSeleccionada;
+            set
+            {
+                semanaSeleccionada = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // ==============================
+        // Tabla
+        // ==============================
+
+        public ObservableCollection<BoletaFila> Filas { get; set; } = new();
+
+
+        // ==============================
+        // Totales
+        // ==============================
+
+        public decimal TotalDevengado => Filas.Sum(f => f.Monto);
+
+        public decimal Descuentos => 0; // placeholder
+
+        public decimal TotalPagar => TotalDevengado - Descuentos;
+
+
+
+#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
+        public BoletaSemanalViewModel() {
+            //Info para la BD
+        }
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
+    }
 }
