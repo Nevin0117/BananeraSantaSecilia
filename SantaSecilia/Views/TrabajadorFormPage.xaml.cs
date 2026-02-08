f36dbc8d2c27cd19720aeedc8f5e99af300bb360
DeisyLMQ <starmoreno02@gmail.com>
2026-02-08 15:24:09 -0500

    Pagina del Registrar Trabajador modificada. Diseño de Figma consistente!!. Con navegación entre pantallas disponibles.

--- SantaSecilia/Views/TrabajadorFormPage.xaml.cs
+++ SantaSecilia/Views/TrabajadorFormPage.xaml.cs
@@ -4,6 +4,10 @@ public partial class TrabajadorFormPage : ContentPage
 {
 	public TrabajadorFormPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+    private async void OnCancelarClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
+    }
 }
