d0dd7f5065502998ca5435b2c32c7afa8cda3a05
CamiloAgr17 <cagrazalesc@gmail.com>
2026-02-09 20:19:28 -0500

    Ajustes para la pantalla de Actividades para evitar errores visuales, se probro funcionalidad del boton creado para registrar una actividad.

--- SantaSecilia/Views/ActividadesPage.xaml.cs
+++ SantaSecilia/Views/ActividadesPage.xaml.cs
@@ -6,4 +6,17 @@ public partial class ActividadesPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void RegistrarActividad_Tapped(object sender, TappedEventArgs e)
+    {
+        var border = (Border)sender;
+
+        // Efecto visual de presionado
+        border.BackgroundColor = Color.FromArgb("#F0F0F0");
+        await Task.Delay(100);
+        border.BackgroundColor = Colors.White;
+
+        // Mensaje al presionar boton
+        await DisplayAlertAsync("Registrar Actividad", "Abriendo ventana...", "OK");
+    }
 }
