b24c7ee223f300bd912a143c5a5f3de5a9b6448d
Leivadev <luis.leivag23@gmail.com>
2026-02-17 21:47:15 -0500

    Refactorizar MauiProgram para agregar inicialización y manejo de migraciones de SQLite

--- SantaSecilia/MauiProgram.cs
+++ SantaSecilia/MauiProgram.cs
@@ -1,25 +1,60 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SantaSecilia.Infrastructure.Data;
 
-namespace SantaSecilia
+namespace SantaSecilia;
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
+        // Asegura que SQLite esté inicializado en todas las plataformas (incluido mobile)
+        SQLitePCL.Batteries_V2.Init();
+
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        builder.Services.AddDbContextFactory<AppDbContext>((_, options) =>
         {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "santa_secilia.db3");
+            options
+                .UseSqlite($"Data Source={dbPath}")
+                .AddInterceptors(new ForeignKeyInterceptor());
+#if DEBUG
+            options.EnableSensitiveDataLogging().EnableDetailedErrors();
+#endif
+        });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
+        var app = builder.Build();
+
+        // Aplica migraciones al iniciar la aplicación (si las hay)
+        ApplyMigrationsAsync(app.Services).GetAwaiter().GetResult();
+
+        return app;
+    }
+
+    private static async Task ApplyMigrationsAsync(IServiceProvider services)
+    {
+        await using var scope = services.CreateAsyncScope();
+        var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+        await using var db = await factory.CreateDbContextAsync();
+        try
+        {
+            await db.Database.MigrateAsync();
+        }
+        catch
+        {
+            // Si ocurre un error durante la migración, intenta crear la base de datos desde cero
+            await db.Database.EnsureCreatedAsync();
         }
     }
 }
