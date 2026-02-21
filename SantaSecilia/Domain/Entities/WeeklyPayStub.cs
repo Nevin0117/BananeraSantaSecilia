f19e770e027e492813a1d30436dcd4f3a07bdc08
Luis Leiva <luis.leiva@utp.ac.pa>
2026-02-21 01:24:34 -0500

    refactorizar tabla de boleta de pago semanal reemplazando weeklyClosing table

--- SantaSecilia/Domain/Entities/WeeklyPayStub.cs
+++ SantaSecilia/Domain/Entities/WeeklyPayStub.cs
@@ -2,18 +2,18 @@ namespace SantaSecilia.Domain.Entities;
 public class WeeklyPayStub
 {
     public int Id { get; set; }
-    public int WeeklyClosingId { get; set; }
     public int WorkerId { get; set; }
+    public DateOnly WeekStart { get; set; }
+    public DateOnly WeekEnd { get; set; }
     public int TotalHours { get; set; }
     public decimal GrossEarnings { get; set; }
-    public decimal SsDeduction { get; set; }
-    public decimal SeDeduction { get; set; }
+    public decimal SocialSecurityDeduction { get; set; }
+    public decimal EducationInsuranceDeduction { get; set; }
     public decimal UnionDues { get; set; }
     public decimal TotalDeductions { get; set; }
     public decimal NetPay { get; set; }
     public DateTime GeneratedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
     public string? PdfPath { get; set; }
-    public WeeklyClosing WeeklyClosing { get; set; } = null!;
     public Worker Worker { get; set; } = null!;
-    public ICollection<PayStubLine> Lines { get; set; } = [];
 }
