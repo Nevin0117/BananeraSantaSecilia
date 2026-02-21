6acdcce647ed81a62ee2ddc7ca96aaac4b02d529
Luis Leiva <luis.leiva@utp.ac.pa>
2026-02-21 01:23:16 -0500

    eliminar tablas innecesarias

--- SantaSecilia/Domain/Entities/EntityType.cs
+++
@@ -1,8 +0,0 @@
-namespace SantaSecilia.Domain.Entities;
-
-public class EntityType
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public ICollection<AuditEvent> AuditEvents { get; set; } = [];
-}
