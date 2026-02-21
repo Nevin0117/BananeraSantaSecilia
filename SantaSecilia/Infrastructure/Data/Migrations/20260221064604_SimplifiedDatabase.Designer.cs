831856cf0d976bea19287be57730e7ce1878c456
Luis Leiva <luis.leiva@utp.ac.pa>
2026-02-21 01:46:50 -0500

    crear migracion de base de datos

--- SantaSecilia/Infrastructure/Data/Migrations/20260221064604_SimplifiedDatabase.Designer.cs
+++ SantaSecilia/Infrastructure/Data/Migrations/20260221064604_SimplifiedDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SantaSecilia.Infrastructure.Data;
 
@@ -10,9 +11,11 @@ using SantaSecilia.Infrastructure.Data;
 namespace SantaSecilia.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260221064604_SimplifiedDatabase")]
+    partial class SimplifiedDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
